09fcbadeb3bb22dcc9e667de9e002a1f9539a21b
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-04-16 19:13:08 +0100

    creating render_map.c

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 18:58:51 by tiagovr4          #+#    #+#             */
-/*   Updated: 2025/04/15 11:56:28 by tiagovr4         ###   ########.fr       */
+/*   Updated: 2025/04/16 13:00:38 by tiagovr4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 int	main (int ac, char **av)
 {
-	// t_game game;
+	t_game game;
 	char	**map;
 	int		i;
 
 	if(ac != 2)
 	{
-		ft_printf("Error: Please insert ""file"".ber\n");
+		ft_putstr_fd("Error: Please insert ""file"".ber\n", 2);
 		return (1);
 	}
 	map = read_map(av[1]);
 	if (!map)
 	{
-		ft_printf("Error: Can't read the map\n");
+		ft_putstr_fd("Error: Failed to read map\n", 2);
 		return (1);
 	}
 	if (!validate_map(map))
 		return (1);
 	i = 0;
+	if (!init_game(game, map))
+	{
+
+	}
 	while (map[i])
 	{
 		ft_printf("%s", map[i]);
---
+++ src/render_map.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 13:45:42 by tiagovr4          #+#    #+#             */
+/*   Updated: 2025/04/16 14:05:04 by tiagovr4         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+// This function renders a single tile on the map
+static void	render_tiles(t_game *game, int x, int y)
+{
+	mlx_put_image_to_window(game->mlx, game->win, game->img_floor, x * 64, y & 64); // Always draw the floor first
+	if (game->map[x][y] == '1')
+		mlx_put_image_to_window(game->mlx, game->win, game->img_wall, x * 64, y * 64);
+	else if (game->map[x][y] == 'C')
+		mlx_put_image_to_window(game->mlx, game->win, game->img_collect, x * 64, y * 64);
+	else if (game->map[x][y] == 'E')
+		mlx_put_image_to_window(game->mlx, game->win, game->img_exit, x * 64, y * 64);
+	else if (game->map[x][y] == 'P')
+		mlx_put_image_to_window(game->mlx, game->win, game->img_player, x * 64, y * 64);
+}
+
+// This function renders the entire map
+void	render_map(t_game *game)
+{
+	char	*moves_str;
+	int		x;
+	int		y;
+
+	y = 0;
+	moves_str = ft_itoa(game->moves);
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			render_tile(game, x, y);
+			x++;
+		}
+		y++;
+	}
+	mlx_string_put(game->mlx, game->win, 10, 20, 0xFFFFFF, "Moves: ");
+	mlx_string_put(game->mlx, game->win, 70, 20, 0xFFFFFF, moves_str);
+	free(moves_str);
+}
