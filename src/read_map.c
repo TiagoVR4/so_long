ef6771a13d3b169934d89bef5874e0a989f554aa
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-04-24 18:14:50 +0100

    add a function to clean the map before is close

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:50:57 by tiagovr4          #+#    #+#             */
-/*   Updated: 2025/04/17 20:20:37 by tiagovr4         ###   ########.fr       */
+/*   Updated: 2025/04/24 18:10:01 by tiagovr4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,19 @@ static char	**init_map(const char *filename, int *lines, int *fd)
 	return (map);
 }
 
+// This function clears the stash after reading the file
+void	gnl_clear_stash(int fd)
+{
+	char	*line;
+	
+	line = get_next_line(fd);
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
+}
+
 char	**read_map(const char *filename)
 {
 	char	**map;
@@ -89,6 +102,7 @@ char	**read_map(const char *filename)
 		i++;
 	}
 	map[i] = NULL;
+	gnl_clear_stash(fd);
 	close(fd);
 	return (map);
 }
