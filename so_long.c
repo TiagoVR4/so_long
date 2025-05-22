07716a7f58166d5259e03ef5055e17c17f688c95
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-05-22 12:04:51 +0100

    add validation to see if the map's extension is right

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 18:58:51 by tiagovr4          #+#    #+#             */
-/*   Updated: 2025/05/07 18:04:09 by tiagovr4         ###   ########.fr       */
+/*   Updated: 2025/05/22 12:01:33 by tiagovr4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,14 @@ static int	handle_error(char *msg, char **map, t_game *game, int free_mlx)
 	return (1);
 }
 
+static int	is_ber(const char *filename)
+{
+	int	len;
+
+	len = ft_strlen(filename);
+	return (len > 4 && !ft_strncmp(filename + len - 4, ".ber", 4));
+}
+
 int	main(int ac, char **av)
 {
 	t_game	game;
@@ -34,6 +42,8 @@ int	main(int ac, char **av)
 	if(ac != 2)
 		return (handle_error("Error: Please insert \"file\".ber\n", NULL, NULL, 0));
 	ft_memset(&game, 0, sizeof(t_game));
+	if (!is_ber(av[1]))
+		return (handle_error("Error: File must have .ber extension\n", NULL, NULL, 0));
 	map = read_map(av[1]);
 	if (!map)
 		return (handle_error("Error: Failed to read map\n", NULL, NULL, 0));
